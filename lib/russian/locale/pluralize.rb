b4106feb8d9f83496a8b1ec33861c0b26654cba1
Artyom Silivonchik <mr.hant@gmail.com>
2011-03-10 13:01:18 +0200

    Symbols for :count patch

--- lib/russian/locale/pluralize.rb
+++ lib/russian/locale/pluralize.rb
@@ -18,14 +18,18 @@
       #   :few  = 2-4, 22-24, 32-34...
       #   :many = 0, 5-20, 25-30, 35-40...
       #   :other = 1.31, 2.31, 5.31...
-      if n % 10 == 1 && n % 100 != 11
-        :one
-      elsif [2, 3, 4].include?(n % 10) && ![12, 13, 14].include?(n % 100)
-        :few
-      elsif n % 10 == 0 || [5, 6, 7, 8, 9].include?(n % 10) || [11, 12, 13, 14].include?(n % 100)
-        :many
+      if (n.class.to_s == "Symbol") then
+        n
       else
-        :other
+        if n % 10 == 1 && n % 100 != 11
+          :one
+        elsif [2, 3, 4].include?(n % 10) && ![12, 13, 14].include?(n % 100)
+          :few
+        elsif n % 10 == 0 || [5, 6, 7, 8, 9].include?(n % 10) || [11, 12, 13, 14].include?(n % 100)
+          :many
+        else
+          :other
+        end
       end
     }
   }
